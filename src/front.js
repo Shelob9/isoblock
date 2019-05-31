cfcec4ba659dfbdaebf2f3717aeca854f5940dfd
Josh Pollock <josh@calderawp.com>
2019-05-31 11:32:31 -0400

    use context to make form edittable in front-end

--- src/front.js
+++ src/front.js
@@ -1,26 +1,95 @@
 import {hydrate} from 'react-dom';
 import {Form} from "./componets/Form";
-import {useState} from '@wordpress/element';
+import {useState, createContext, useContext} from '@wordpress/element';
+
+/**
+ * All DOM elements with block
+ * @type {NodeListOf<Element>}
+ */
 const elements = document.querySelectorAll('.wp-block-hydrate-test');
-const App = ({defaultValue,id,onSubmit}) => {
-    const [value,setValue] = useState(defaultValue);
+
+/**
+ * Create a context
+ *
+ * @see https://daveceddia.com/usecontext-hook/
+ * @see https://reactjs.org/docs/context.html
+ * @type {React.Context<any>}
+ */
+const FormContext = createContext();
+const {Provider} = FormContext;
+
+/**
+ * Provide data for the form
+ *
+ * @param initalValue
+ * @param children
+ * @returns {*}
+ * @constructor
+ */
+function FormDataProvider({initalValue, children}) {
+    const [defaultValue, setValue] = useState(initalValue);
+    const onChange = (event) => {
+        setValue(event.target.value)
+    };
     return (
-        <Form
-            onChange={onChange}
-            defaultValue={defaultValue}
-            id={id}
-            onSubmit={onSubmit}
-        />
+        <Provider value={{
+            defaultValue,
+            onChange
+        }}>
+            {children}
+        </Provider>
     );
+}
+
+/**
+ * Form wrapped in context
+ *
+ * @param id
+ * @returns {*}
+ * @constructor
+ */
+const FormConsuming = ({id}) => {
+    const {
+        defaultValue,
+        onChange
+    } = useContext(FormContext);
+    return (
+        <Form defaultValue={defaultValue} onChange={onChange} id={id}/>
+    );
+
 };
+
+/**
+ * Show form value with context
+ * @returns {*}
+ * @constructor
+ */
+function DisplayConsuming() {
+    const {
+        defaultValue,
+    } = useContext(FormContext);
+    return (
+        <div>{defaultValue}</div>
+
+    )
+}
+
+/**
+ * If we have any blocks hydrate ssr HTML
+ */
 if (elements.length) {
     elements.forEach(element => {
-        const input = element.querySelector( '.hydrate-test-field' );
+        const input = element.querySelector('.hydrate-test-field');
         const defaultValue = null !== input ? input.value : 'Hi Roy';
-        const id = null !== input  ? input.id : 'hi-roy';
-        hydrate(<Form
-            defaultValue={defaultValue}
-            id={id}
-        />,element);
+        const id = null !== input ? input.id : 'hi-roy';
+        hydrate(
+            <FormDataProvider initalValue={defaultValue}>
+                <FormConsuming id={id}/>
+                <DisplayConsuming/>
+            </FormDataProvider>
+            , element
+        );
     })
+
+
 }
