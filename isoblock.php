569dda1c49cbd78ed920c3407fd947bcb700a804
Josh Pollock <jpollock412@gmail.com>
2019-06-25 12:01:16 -0400

    use block.json for PHP block registration

--- isoblock.php
+++ isoblock.php
@@ -7,25 +7,69 @@
 /**
  * Load block
  */
-add_action( 'init', function() {
-    $block = new WP_Block_Type( 'josh/isoblock' ,[]);
-    $block->editor_script = 'isoblock-editor';
-    $block->script = 'isoblock-front';
+add_action('init', function () {
 
-    wp_register_script(
-        $block->editor_script,
-        plugins_url( '/build/index.js', __FILE__ ),
-        [ 'wp-blocks', 'wp-element', 'wp-editor' ],
-        md5_file( __DIR__ . '/build/index.js' ),
-        true
-    );
-    wp_register_script(
-        $block->script,
-        plugins_url( '/build/front.js', __FILE__ ),
-        [ 'wp-blocks', 'wp-element', 'wp-editor' ],
-        md5_file( __DIR__ . '/build/front.js' ),
-        true
-    );
+    $blockConfig = json_decode(file_get_contents(__DIR__ . '/block.json'), true);
+    $name = $blockConfig['name'];
 
-    register_block_type( $block );
-} );
+    $block = new WP_Block_Type($name, []);
+    $block->script = str_replace('/', '-', $name);
+    $block->editor_script = str_replace('/', '-', $name) . '-editor';
+
+
+    //Register script
+    if (!empty($blockConfig['script'])) {
+        //setting this prop, causes script to be enqueued with block
+        $block->script = str_replace('/', '-', $name);
+        wp_register_script(
+            $block->script,
+            esc_url_raw(plugins_url($blockConfig['script'], __FILE__)),
+            ['wp-blocks', 'wp-element', 'wp-editor'],
+            md5_file(__DIR__ . '/' . $blockConfig['script']),
+            true
+        );
+    }
+
+    //Register style
+    if (!empty($blockConfig['style'])) {
+        //setting this prop, causes style to be enqueued with block
+        $block->style = str_replace('/', '-', $name) . '-style';
+        wp_register_style(
+            $block->style,
+            esc_url_raw(plugins_url($blockConfig['style'], __FILE__)),
+            [],
+            md5_file(__DIR__ . '/' . $blockConfig['style']),
+            true
+        );
+    }
+
+    //Register editor script
+    if (!empty($blockConfig['editorScript'])) {
+        //setting this prop, causes script to be enqueued with block in editor
+        $block->editor_script = str_replace('/', '-', $name) . '-editor';
+
+        wp_register_script(
+            $block->editor_script,
+            esc_url_raw(plugins_url($blockConfig['editorScript'], __FILE__)),
+            ['wp-blocks', 'wp-element', 'wp-editor'],
+            md5_file(__DIR__ . '/' . $blockConfig['editorScript']),
+            true
+        );
+    }
+
+    //register editor style
+    if (!empty($blockConfig['editorStyle'])) {
+        //setting this prop, causes style to be enqueued with block in editor
+        $block->editor_style = str_replace('/', '-', $name) . '-style';
+        wp_register_style(
+            $block->editor_style,
+            esc_url_raw(plugins_url($blockConfig['editorStyle'], __FILE__)),
+            [],
+            md5_file(__DIR__ . '/' . $blockConfig['editorStyle']),
+            true
+        );
+    }
+
+
+    register_block_type($block);
+});
